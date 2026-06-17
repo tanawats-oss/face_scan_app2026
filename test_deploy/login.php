8505db5495c0c4b4bcc98dd077528f23b5febfbf
tanawats-oss <tanawats@g.swu.ac.th>
2026-06-17 16:21:06 +0700

    Fix All Tranfer Server

--- test_deploy/login.php
+++ test_deploy/login.php
@@ -15,6 +15,11 @@ Developed   : 2025
 <html lang="th">
 
 <head>
+      <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W8ZD4T59');
+    </script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="author" content="นายธนวัฒน์ เสริฐสุวรรณกุล">
     <meta name="developer-email" content="tanawats@g.swu.ac.th">
@@ -107,7 +112,12 @@ Developed   : 2025
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8ZD4T59"height="0" width="0" style="display:none;visibility:hidden">
+            </iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
     <script>
         (function() {
             const ua = navigator.userAgent;
